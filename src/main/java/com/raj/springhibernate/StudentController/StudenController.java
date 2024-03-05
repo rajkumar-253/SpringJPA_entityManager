d4f6f02c5071333f1268c9fd96070ef158adc9c2
raj 253 <rajkumartholam253@gmail.com>
2024-03-05 10:07:21 +0530

    Return types changed with Response Entity Methods

--- src/main/java/com/raj/springhibernate/StudentController/StudenController.java
+++ src/main/java/com/raj/springhibernate/StudentController/StudenController.java
@@ -3,6 +3,9 @@ package com.raj.springhibernate.StudentController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,30 +24,34 @@ public class StudenController {
 	public StudentInterface studentInterface;
 	
 	@PostMapping("createStudent")
-	public StudentDTO createStudent(@RequestBody StudentDTO studentDTO) {
+	public ResponseEntity<StudentDTO> createStudent(@RequestBody StudentDTO studentDTO) {
 		StudentDTO returnStudent= studentInterface.createStudent(studentDTO);
-		return returnStudent;
+		return ResponseEntity.status(HttpStatus.CREATED).body(returnStudent);
 	}
 	@GetMapping("findStudent/{id}")
-	public StudentDTO findStudent(@PathVariable int id) {
-		return studentInterface.findStudent(id);
+	public ResponseEntity<StudentDTO> findStudent(@PathVariable int id) {
+		 StudentDTO student = studentInterface.findStudent(id);
+		 return ResponseEntity.ok().body(student);
 		
 	}
 	
 	@GetMapping("getStudents")
-	public List<StudentDTO> getAllStudetns() {
-		return studentInterface.getAllStudents();
+	public ResponseEntity<List<StudentDTO>> getAllStudetns() {
+		 List<StudentDTO> allStudents = studentInterface.getAllStudents();
+		 return ResponseEntity.ok(allStudents);
 		
 	}
 	
 	@PutMapping("updateStudent")
-	public StudentDTO updateStudent(@RequestBody StudentDTO studentDTO) {
-		return studentInterface.updateStudent(studentDTO);
+	public ResponseEntity<StudentDTO> updateStudent(@RequestBody StudentDTO studentDTO) {
+		 StudentDTO updateStudent = studentInterface.updateStudent(studentDTO);
+		 return ResponseEntity.status(HttpStatus.OK).body(updateStudent);
 	}
 	
-	@DeleteMapping("deleteMapping/{id}")
-	public void deleteMapping(@PathVariable int id) {
+	@DeleteMapping("deleteStudent/{id}")
+	public ResponseEntity<String> deleteMapping(@PathVariable int id) {
 		 studentInterface.deleteStudent(id);
+		 return ResponseEntity.ok().body("Successfully Deleted");
 	}
 
 }
